d45118e596affcc0b1f6f3d63625e8a4f0f0acda
Robbie1977 <robbie.court@gmail.com>
2014-03-07 10:24:52 +0000

    adding script

---
+++ getFlybase.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Fetching flybase DB from ftp://ftp.flybase.net/releases/current/psql/"
+cd /disk/data/VFB/scripts/robbie/flybase/
+rm *.gz.*
+wget ftp://ftp.flybase.net/releases/current/psql/*.gz.*
+createdb -E UTF-8 -h localhost -U nmilyav1 flybase_new
+cat *.gz* | gunzip | psql -h localhost -U nmilyav1 flybase_new
+vacuumdb -f -z -v flybase_new -U nmilyav1 -h localhost
+psql -h localhost -U nmilyav1 nmilyav1 -c "ALTER DATABASE 'flybase' RENAME TO flybase_old"
+psql -h localhost -U nmilyav1 nmilyav1 -c "ALTER DATABASE 'flybase_new' RENAME TO flybase"
+psql -h localhost -U flybase flybase < /disk/data/VFB/Chado/scripts/chado_views_for_vfb.sql
