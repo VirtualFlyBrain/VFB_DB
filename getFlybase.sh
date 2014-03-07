1ced838478353eafb734202172763785236e325e
Robbie1977 <robbie.court@gmail.com>
2014-03-07 10:54:00 +0000

    Update getFlybase.sh

--- getFlybase.sh
+++ getFlybase.sh
@@ -8,4 +8,4 @@ cat *.gz* | gunzip | psql -h localhost -U nmilyav1 flybase_new
 vacuumdb -f -z -v flybase_new -U nmilyav1 -h localhost
 psql -h localhost -U nmilyav1 nmilyav1 -c "ALTER DATABASE 'flybase' RENAME TO flybase_old"
 psql -h localhost -U nmilyav1 nmilyav1 -c "ALTER DATABASE 'flybase_new' RENAME TO flybase"
-psql -h localhost -U flybase flybase < /disk/data/VFB/Chado/scripts/chado_views_for_vfb.sql
+psql -h localhost -U flybase flybase < TableQueries/chado_views_for_vfb.sql
