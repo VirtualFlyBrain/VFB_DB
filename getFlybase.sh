1b5c6e5f3f6c3950e7303ab675a98f56aed227c0
Robert Court <robbie.court@gmail.com>
2014-03-20 20:44:29 +0000

    adding user clearing
    enables database to be changed over

--- getFlybase.sh
+++ getFlybase.sh
@@ -13,14 +13,18 @@ createdb -E UTF-8 -h localhost -U nmilyav1 flybase_new
 cat *.gz* | gunzip | psql -h localhost -U nmilyav1 flybase_new
 vacuumdb -f -z -v flybase_new -U nmilyav1 -h localhost
 psql -h localhost -U nmilyav1 flybase_new -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO flybase"
+
+dropdb -h localhost -U nmilyav1 'flybase_old'
+
 rm ../old/flybase_old.dump
 rm ../old/revision
 /usr/pgsql-9.3/bin/pg_dump -h localhost -U nmilyav1 flybase > ../old/flybase_old.dump
 mv revision ../old/
 rm flybase.dump
 ls *.gz.00 | rev | cut -c 11- | rev > revision
-/usr/pgsql-9.3/bin/pg_dump -h localhost -U nmilyav1 flybase_new > flybase.dump
-psql -h localhost -U nmilyav1 flybase < flybase.dump
+psql -h localhost -U nmilyav1 flybase -c "SELECT usename, pid FROM pg_stat_activity WHERE datname = current_database();"
+psql -h localhost -U nmilyav1 postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='flybase';"
+psql -h localhost -U nmilyav1 postgres -c "ALTER DATABASE 'flybase' RENAME TO flybase_old"
+psql -h localhost -U nmilyav1 postgres -c "ALTER DATABASE 'flybase_new' RENAME TO flybase"
 vacuumdb -f -z -v flybase -U nmilyav1 -h localhost
 psql -h localhost -U flybase flybase < TableQueries/chado_views_for_vfb.sql
-dropdb -h localhost -U nmilyav1 'flybase_new'
