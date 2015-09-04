facda07a178e838828d61db029f37e686d27e417
Robert Court <robbie.court@gmail.com>
2015-09-04 13:54:03 +0100

    adding release date from file data

--- getFlybase.sh
+++ getFlybase.sh
@@ -3,7 +3,7 @@
 # localhost:5432:*:flybase:password
 # localhost:5432:*:jenkins:password
 # Obviously replacing password with the real password.
-# Note: This will take hours! 
+# Note: This will take hours!
 # Note: You may need to change pgsql revision for pg_dump commands if server is updated or not run on main server.
 echo "Fetching flybase DB from ftp://ftp.flybase.net/releases/current/psql/"
 cd current
@@ -17,18 +17,18 @@ if [ `ls *.gz.00 | rev | cut -c 11- | rev` != `cat revision` ]
 then
 
   # clean flybase_new: just incase - should fail.
-  
+
   psql -h localhost -U jenkins flybase_new -c "SELECT usename, pid FROM pg_stat_activity WHERE datname = current_database();"
   psql -h localhost -U jenkins postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='flybase_new';"
-  
-  dropdb -h localhost -U jenkins 'flybase_new' 
+
+  dropdb -h localhost -U jenkins 'flybase_new'
   createdb -E UTF-8 -h localhost -U jenkins flybase_new
   cat *.gz* | unpigz | psql -h localhost -U jenkins flybase_new
   vacuumdb -f -z -v flybase_new -U jenkins -h localhost
   psql -h localhost -U jenkins flybase_new -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO flybase"
-  
+
   dropdb -h localhost -U jenkins 'flybase_old'
-  
+
   rm ../old/flybase_old.dump
   rm ../old/revision
   if [ `tail -n 1 /etc/hosts | rev | cut -c -6 | rev` == 'blanik' ]
@@ -54,17 +54,17 @@ then
   mv revision ../old/
   echo "FB DB updating..." > revision
   rm flybase.dump
-  
+
   psql -h localhost -U jenkins flybase -c "SELECT usename, pid FROM pg_stat_activity WHERE datname = current_database();"
   psql -h localhost -U jenkins postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='flybase';"
-  
+
   psql -h localhost -U jenkins postgres -c "ALTER DATABASE flybase RENAME TO flybase_old"
   psql -h localhost -U jenkins postgres -c "ALTER DATABASE flybase_new RENAME TO flybase"
   echo 'DB swapped'
-  
+
   echo `date`
   echo New DB online.
-  ls *.gz.00 | rev | cut -c 11- | rev > revision
+  echo $(ls *.gz.00 | rev | cut -c 11- | rev) released on $(date -r *.gz.00 +'%d/%m/%y') > revision
 
 else
   echo "No Change!"
