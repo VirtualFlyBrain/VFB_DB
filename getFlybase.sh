139d33073cea854d9de284ba10abded3c60dabf5
Rob Court <rcourt@ed.ac.uk>
2018-05-09 19:00:41 +0100

    swapping to remote server

--- getFlybase.sh
+++ getFlybase.sh
@@ -17,48 +17,48 @@ if [ `ls *.gz.00 | rev | cut -c 11- | rev` != `cat revision | cut -c -9` ]
 then
 
   # clean flybase_new: just incase - should fail.
-  psql -h localhost -U jenkins flybase_new -c "SELECT usename, pid FROM pg_stat_activity WHERE datname = current_database();"
-  psql -h localhost -U jenkins postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='flybase_new';"
+  psql -h ${PSSERVER} -U jenkins flybase_new -c "SELECT usename, pid FROM pg_stat_activity WHERE datname = current_database();"
+  psql -h ${PSSERVER} -U jenkins postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='flybase_new';"
 
-  dropdb -h localhost -U jenkins 'flybase_new'
-  createdb -E UTF-8 -h localhost -U jenkins flybase_new
-  cat *.gz* | unpigz | psql -h localhost -U jenkins flybase_new 2>&1
-  vacuumdb -f -z -v flybase_new -U jenkins -h localhost
-  psql -h localhost -U jenkins flybase_new -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO flybase"
+  dropdb -h ${PSSERVER} -U jenkins 'flybase_new'
+  createdb -E UTF-8 -h ${PSSERVER} -U jenkins flybase_new
+  cat *.gz* | unpigz | psql -h ${PSSERVER} -U jenkins flybase_new 2>&1
+  vacuumdb -f -z -v flybase_new -U jenkins -h ${PSSERVER}
+  psql -h ${PSSERVER} -U jenkins flybase_new -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO flybase"
 
-  dropdb -h localhost -U jenkins 'flybase_old'
+  dropdb -h ${PSSERVER} -U jenkins 'flybase_old'
 
   rm ../old/flybase_old.dump
   rm ../old/revision
   if [ `tail -n 1 /etc/hosts | rev | cut -c -6 | rev` == 'blanik' ]
   then
-    pg_dump -h localhost -U jenkins flybase > ../old/flybase_old.dump
+    pg_dump -h ${PSSERVER} -U jenkins flybase > ../old/flybase_old.dump
   else
-    pg_dump -h localhost -U jenkins flybase > ../old/flybase_old.dump
+    pg_dump -h ${PSSERVER} -U jenkins flybase > ../old/flybase_old.dump
   fi
   echo `date`
   echo 'Stating vacuum...'
-  vacuumdb -f -z -v flybase_new -U jenkins -h localhost
+  vacuumdb -f -z -v flybase_new -U jenkins -h ${PSSERVER}
   echo 'Finished vacuum.'
   echo `date`
   echo 'Staring creating custom tables (long process with no feedback) ...'
-  psql -h localhost -U jenkins flybase_new < ../TableQueries/chado_views_for_vfb.sql
+  psql -h ${PSSERVER} -U jenkins flybase_new < ../TableQueries/chado_views_for_vfb.sql
   echo 'Finished creating views.'
-  psql -h localhost -U jenkins flybase_new -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO flybase"
+  psql -h ${PSSERVER} -U jenkins flybase_new -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO flybase"
   echo `date`
   echo 'Stating vacuum...'
-  vacuumdb -f -z -v flybase_new -U jenkins -h localhost
+  vacuumdb -f -z -v flybase_new -U jenkins -h ${PSSERVER}
   echo 'Finished vacuum.'
   echo `date`
   mv revision ../old/
   echo "FB DB updating..." > revision
   rm flybase.dump
 
-  psql -h localhost -U jenkins flybase -c "SELECT usename, pid FROM pg_stat_activity WHERE datname = current_database();"
-  psql -h localhost -U jenkins postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='flybase';"
+  psql -h ${PSSERVER} -U jenkins flybase -c "SELECT usename, pid FROM pg_stat_activity WHERE datname = current_database();"
+  psql -h ${PSSERVER} -U jenkins postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='flybase';"
 
-  psql -h localhost -U jenkins postgres -c "ALTER DATABASE flybase RENAME TO flybase_old"
-  psql -h localhost -U jenkins postgres -c "ALTER DATABASE flybase_new RENAME TO flybase"
+  psql -h ${PSSERVER} -U jenkins postgres -c "ALTER DATABASE flybase RENAME TO flybase_old"
+  psql -h ${PSSERVER} -U jenkins postgres -c "ALTER DATABASE flybase_new RENAME TO flybase"
   echo 'DB swapped'
 
   echo `date`
